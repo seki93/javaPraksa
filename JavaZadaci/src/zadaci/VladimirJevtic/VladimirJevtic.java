2882d9b572e0cf1424f22bff870ee86b0a9311fb
VladimirJevtic <vladimirjevtic1995@hotmail.com>
2018-02-06 04:21:20 -0800

    Drugaciji Pristup, fali jedan System Out

---
+++ JavaZadaci/src/zadaci/VladimirJevtic/VladimirJevtic.java
@@ -0,0 +1,31 @@
+
+public class VladimirJevtic {
+	
+
+		public static void main(String[] args) {
+			
+			String dobarPrimer = "asadaffghdfa";
+			String losPrimer  = "asdfghjk";
+			
+			
+			System.out.println();
+	  		// pokusao sam drugaciji pristup, preko brojaca
+			int brojac = 0;
+			// Menjamo losPrimer/dobarPrimer
+			char [] ulaz = dobarPrimer.toCharArray();
+			System.out.println(" Duplikati: ");
+			for (int i = 0; i < dobarPrimer.length(); i++) {
+				for (int j = i + 1 ; j< dobarPrimer.length(); j++) {
+					if (ulaz[i] == ulaz[j]) {
+						System.out.println(ulaz[j]);
+						brojac++;
+						break;
+						
+					}
+					}
+					
+					}
+				
+				}
+			}
+				
