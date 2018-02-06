1a82f58a635f447d684c2da1efc63aef11ac5402
RajkoJegdic <rajkojegdic@yahoo.com>
2018-02-06 12:10:09 +0100

    Dodat java projekat

---
+++ Stringic/src/GlavnaKlasa.java
@@ -0,0 +1,14 @@
+
+public class GlavnaKlasa {
+
+	public static void main(String[] args) {
+		
+		String dobarPrimer = "asadaffghdfa";
+		String losPrimer  = "asdfghjk";
+		
+		
+		System.out.println();
+  		
+	}
+
+}
