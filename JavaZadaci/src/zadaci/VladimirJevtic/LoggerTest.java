5ca427ffb1e276e3db35cfa51dce562ef66c034f
VladimirJevtic <vladimirjevtic1995@hotmail.com>
2018-02-07 12:59:52 +0100

    LoggerProveraINivoi

---
+++ JavaZadaci/src/zadaci/VladimirJevtic/LoggerTest.java
@@ -0,0 +1,14 @@
+package zadaci.VladimirJevtic;
+import org.apache.log4j.*;
+
+public class LoggerTest {
+	
+	private static final Logger Log = Logger.getLogger(LoggerTest.class);
+	
+	public static void main(String[] args ) {
+		
+		Log.info(" Provera Loggera " );
+		
+	}
+
+}
