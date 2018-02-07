8540ab4dd9294df6cc74501145dd52657104f32f
ZekaJava <36188380+ZekaJava@users.noreply.github.com>
2018-02-07 13:17:14 +0100

    ProveraZeka

---
+++ JavaZadaci/src/zadaci/Aleksandar/LoggerTest.java
@@ -0,0 +1,15 @@
+package zadaci.Aleksandar;
+
+import org.apache.log4j.*;
+
+public class LoggerTest {
+
+	private static final Logger logger = Logger.getLogger(LoggerTest.class);
+	
+	public static void main(String[] args) {
+		
+		logger.info("Provera Loggera");
+
+	}
+
+}
