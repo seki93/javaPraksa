9360a8c3a87afe13deaba754887c1d445f94091f
ZekaJava <36188380+ZekaJava@users.noreply.github.com>
2018-02-07 13:29:37 +0100

    ZekaNovaProvera

--- JavaZadaci/src/zadaci/Aleksandar/LoggerTest.java
+++ JavaZadaci/src/zadaci/Aleksandar/LoggerTest.java
@@ -2,14 +2,32 @@ package zadaci.Aleksandar;
 
 import org.apache.log4j.*;
 
-public class LoggerTest {
+public class LoggerTest 
+{
 
 	private static final Logger logger = Logger.getLogger(LoggerTest.class);
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
-		logger.info("Provera Loggera");
+		LoggerTest logger = new LoggerTest();
+		logger.pokreni("Provera");
 
+	}
+	
+	private void pokreni(String poruka) 
+	{
+		if(logger.isDebugEnabled()){
+			logger.debug("Ovo je debug : " + poruka);
+		}
+
+		if(logger.isInfoEnabled()){
+			logger.info("Ovo je info : " + poruka);
+		}
+
+		logger.warn("Ovo je warn : " + poruka);
+		logger.error("ovo je error : " + poruka);
+		logger.fatal("Ovo je fatal : " + poruka);
 	}
 
 }
