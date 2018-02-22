35d5ca2e7a2638c26a141326892c649f253e7e33
seki93 <ssekeres21@gmail.com>
2018-02-22 14:58:03 +0100

    StringAnagramInOrder Sekres

--- JavaZadaci/src/zadaci/SekeresStefan/StringAnagrama.java
+++ JavaZadaci/src/zadaci/SekeresStefan/StringAnagrama.java
@@ -1,5 +1,69 @@
 package zadaci.SekeresStefan;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class StringAnagrama {
+	
+	public static void main(String[] args) {
+		
+		ArrayList<String> anagramsInOrder = new ArrayList<String>();
+		ArrayList<String> newList = new ArrayList<String>();
+		anagramsInOrder.add("abcd");
+		anagramsInOrder.add("cfrtg");
+		anagramsInOrder.add("dcba");
+		anagramsInOrder.add("dfrt");
+		anagramsInOrder.add("frtd");
+		anagramsInOrder.add("amnj");
+		anagramsInOrder.add("retulkj");
+		anagramsInOrder.add("rftd");
+		
+		changeStringOder(anagramsInOrder, newList);
+		System.out.println(newList);
+		//System.out.println(""+isAnagram("abcd", "cfrtg"));
+		
+	}
+	
+	
+	public static void changeStringOder(ArrayList<String> anagram, ArrayList<String> newList) {
+		
+		for( int i = 0; i < anagram.size(); i++) {
+			newList.add(anagram.get(i));
+			for( int j = i + 1; j < anagram.size(); j++ ) {
+				if(isAnagram(anagram.get(i), anagram.get(j))) {
+					newList.add(anagram.get(j));
+					anagram.remove(anagram.get(j));
+				}
+			}
+		}
+		
+	}
+	
+	public static boolean isAnagram(String a, String b) {
+		
+		HashMap<Character, Integer> map = new HashMap<>();
+		
+		if( a.length() != b.length()) return false;
+		
+		for( int i = 0; i < a.length(); i++) {
+			if(map.containsKey(a.charAt(i))) {
+				map.put(a.charAt(i), map.get(a.charAt(i)) + 1);
+			}else {
+				map.put(a.charAt(i), 1);
+			}
+		}
+		
+		for( int i = 0; i < b.length(); i++) {
+			if(!map.containsKey(b.charAt(i))) return false;
+			else {
+				map.put(b.charAt(i), map.get(b.charAt(i)) - 1);
+			
+				if(map.get(b.charAt(i)) == 0)
+					map.remove(b.charAt(i));
+			}
+		}
+		
+		return map.isEmpty();
+	}
 
 }
