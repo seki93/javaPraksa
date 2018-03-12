74f2dfe58585720750edaaa6f5d76cdc73a10455
VladimirJevtic <vladimirjevtic1995@hotmail.com>
2018-03-12 11:20:22 +0100

    Cistio Importe

--- Spring/src/main/java/hello/service/ClubServiceImp.java
+++ Spring/src/main/java/hello/service/ClubServiceImp.java
@@ -4,12 +4,10 @@ import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Primary;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import hello.model.Club;
 import hello.repository.ClubRepository;
-import hello.repository.CustomClubRepository;
 
 @Service
 @Primary
