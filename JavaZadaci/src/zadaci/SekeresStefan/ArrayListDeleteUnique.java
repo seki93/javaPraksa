0864f6eef5649bf32293893f9517ed36e2f25abe
seki93 <ssekeres21@gmail.com>
2018-03-12 15:34:20 +0100

    DeleteUnique - Sekeres

---
+++ JavaZadaci/src/zadaci/SekeresStefan/ArrayListDeleteUnique.java
@@ -0,0 +1,66 @@
+package zadaci.SekeresStefan;
+
+import java.util.ArrayList;
+
+public class ArrayListDeleteUnique {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		list.add("1");
+		list.add("x");
+		list.add("b");
+		list.add("b");
+		list.add("a");
+		list.add("c");
+		list.add("c");
+		list.add("a");
+		list.add("2");
+		list.add("1");
+		list.add("c");
+		list.add("4");
+		list.add("5");
+		list.add("5");
+		list.add("4");
+		list.add("4");
+		
+		System.out.println("List before method:");
+		System.out.println(list);
+		
+		list = deleteUnique(list);
+		System.out.println("After: ");
+		System.out.println(list);
+		
+		
+	}
+	
+	
+	public static ArrayList<String> deleteUnique(ArrayList<String> list) {
+		ArrayList<String> sec = new ArrayList<>();
+		
+		boolean exists = false;
+		
+		for( int i = 0; i < list.size(); i++) {
+			int j = i + 1;
+			
+			while( j < list.size()) {
+				if( list.get(i) == list.get(j)) {
+					exists = true;
+					sec.add(list.get(j));
+					list.remove(j);
+					j--;
+				}
+				j++;
+			}
+			
+			if(exists) {
+				sec.add(list.get(i));
+			}
+			
+			exists = false;
+		}
+		
+		return  sec;
+	}
+}
