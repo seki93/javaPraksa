62677c0697c597e6756095ecc31605ffb34baf56
VladimirJevtic <vladimirjevtic1995@hotmail.com>
2018-04-02 14:28:47 +0200

    Popravljen da vraca niz preko metode a ne void

--- JavaZadaci/src/zadaci/VladimirJevtic/ParalelniNizoviSort.java
+++ JavaZadaci/src/zadaci/VladimirJevtic/ParalelniNizoviSort.java
@@ -1,5 +1,6 @@
 package zadaci.VladimirJevtic;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 import org.apache.log4j.Logger;
@@ -8,16 +9,33 @@ public class ParalelniNizoviSort {
 	
 	private static final Logger LOGGER = Logger.getLogger(ParalelniNizoviSort.class.getName() );
 	
-	public static void metoda(int vrednost[], int indeksi[]){
+	public static int[] metoda(int vrednost[], int indeksi[]){
 		
-		HashMap<Integer, Integer> mapa = new HashMap<Integer, Integer>();
+//		HashMap<Integer, Integer> mapa = new HashMap<Integer, Integer>();
+//		
+//		for (int i = 0; i < vrednost.length; i++) {
+//			if (vrednost != null && indeksi != null)
+//				mapa.put(indeksi[i], vrednost[i]);
+//		}
+//		 
+//		LOGGER.debug(mapa.values());
 		
 		for (int i = 0; i < vrednost.length; i++) {
-			if (vrednost != null && indeksi != null)
-				mapa.put(indeksi[i], vrednost[i]);
+			for (int j = i +1; j < vrednost.length; j++) {
+			
+			if (indeksi[i] > indeksi[j]) {
+				int temp = indeksi[i];
+				indeksi[i] = indeksi[j];
+				indeksi[j] = temp;
+				
+				int temp2 = vrednost[i];
+				vrednost[i] = vrednost[j];
+				vrednost[j] = temp2;
+				
+				}
+			}
 		}
-		
-		LOGGER.debug(mapa.values());
+		return vrednost;
 		
 	}
 
@@ -25,8 +43,8 @@ public class ParalelniNizoviSort {
 		
 		int vrednosti[] = {20, 11, 45, 32, 31, 68};
 		int indeksi[] = {5, 0, 2, 1, 4, 3};
+		LOGGER.debug(Arrays.toString(metoda(vrednosti, indeksi)));
 		
-		metoda(vrednosti, indeksi);
 		
 
 	}
