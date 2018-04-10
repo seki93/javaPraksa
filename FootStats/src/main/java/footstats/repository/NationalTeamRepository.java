059b32ff96d0c9497d64dfa041d6ab4821320bf8
VladimirJevtic <vladimirjevtic1995@hotmail.com>
2018-04-10 14:51:56 +0200

    Country, Match, MatchStats, NationalTeam Repository dodat

---
+++ FootStats/src/main/java/footstats/repository/NationalTeamRepository.java
@@ -0,0 +1,7 @@
+package footstats.repository;
+
+import footstats.model.NationalTeam;
+import org.springframework.data.repository.CrudRepository;
+
+public interface NationalTeamRepository extends CrudRepository<NationalTeam, Integer> {
+}
