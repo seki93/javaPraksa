f496311d56675e3a9cf3fca59b7c790204b973d7
VladimirJevtic <vladimirjevtic1995@hotmail.com>
2018-04-10 14:08:12 +0200

    Prekopirao Diff ChangeLog u liquibase ChangeLog

--- FootStats/target/liquibase/migrate.sql
+++ FootStats/target/liquibase/migrate.sql
@@ -2,13 +2,13 @@
 --  SQL to add all changesets to database history table
 --  *********************************************************************
 --  Change Log: src/main/resources/liquibase-changeLog.xml
---  Ran at: 4/10/18 1:55 PM
+--  Ran at: 4/10/18 2:00 PM
 --  Against: root@localhost@jdbc:mysql://localhost:3306/footstats
 --  Liquibase version: 3.4.1
 --  *********************************************************************
 
 --  Lock Database
-UPDATE DATABASECHANGELOGLOCK SET LOCKED = 1, LOCKEDBY = 'INTERN-RAJKOJ (192.168.52.66)', LOCKGRANTED = '2018-04-10 13:55:37.251' WHERE ID = 1 AND LOCKED = 0;
+UPDATE DATABASECHANGELOGLOCK SET LOCKED = 1, LOCKEDBY = 'INTERN-JEVTICV (192.168.52.7)', LOCKGRANTED = '2018-04-10 14:00:50.335' WHERE ID = 1 AND LOCKED = 0;
 
 --  Release Database Lock
 UPDATE DATABASECHANGELOGLOCK SET LOCKED = 0, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
