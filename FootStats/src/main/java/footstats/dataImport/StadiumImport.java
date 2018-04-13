6d6a8e5fea86c0fbb551a405e032e0db96848c3d
seki93 <ssekeres21@gmail.com>
2018-04-13 13:45:51 +0200

    Skimuto 1312 stadiona sa wikipedije - Sekeres

--- FootStats/src/main/java/footstats/dataImport/StadiumImport.java
+++ FootStats/src/main/java/footstats/dataImport/StadiumImport.java
@@ -1,10 +1,12 @@
 package footstats.dataImport;
 
-
-
 import java.io.File;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.util.ArrayList;
+import java.util.List;
 
 
 import org.apache.log4j.Logger;
@@ -27,8 +29,7 @@ public class StadiumImport {
 
     final static Logger log = Logger.getLogger(StadiumImport.class);
 
-    private ArrayList<String> premierLeagueStadiums = new ArrayList<String>();
-    private ArrayList<String> primeraLeagueStadiums = new ArrayList<String>();
+    private ArrayList<String> stadiums = new ArrayList<String>();
 
     @Test
     public void simpleChromeDrivertest() throws InterruptedException{
@@ -36,74 +37,58 @@ public class StadiumImport {
         WebDriver driver= new ChromeDriver();
         log.debug("Opening browser");
         driver.manage().window().maximize();
-        String url = "https://en.wikipedia.org/wiki/2017%E2%80%9318_Premier_League";
-        log.debug("navigating to the "  + url);
+
+        String url = "https://en.wikipedia.org/wiki/List_of_association_football_stadiums_by_country";
         driver.get(url);
 
-        Actions action = new Actions(driver);
-        //action.moveToElement(driver.findElement(By.id("search")));
-       // action.sendKeys(driver.findElement(By.id("search")), "jpa").build().perform();
+        List<WebElement> rows = driver.findElements(By.xpath("//*[@id=\"mw-content-text\"]/div/table[2]/tbody/tr"));
 
-        try {
-            driver.findElement(By.id("search")).submit();
-        } catch (Exception e) {
-            // TODO: handle exception
-        }
-//        // Get the content or container
-//        WebElement content = driver.findElement(By.id("contentText"));
-//
-//        //Get the table of users
-//        WebElement tblUsers = content.findElement(By.xpath(".//table/tbody/tr[2]/td/table/tbody/tr/td[1]/table"));
-//
-//        // Get the rows which change always as and when users are added
-//        WebElement allUsers = tblUsers.findElements(By.xpath(".//tbody/tr"));
-//
-//        // Loop through each row of users table
-//        for(WebElement user : allUsers) {
-//
-//            // Get the username
-//            WebElement username = user.findElement(By.xpath(".//td/table/tbody/tr/td[1]/strong[2]"));
-//            System.out.println("Username: " + username.getText());
-//        }
-
-        Thread.sleep(5000);
-        System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-        //System.out.println(driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table[2]/tbody")).getText());
-        //PREMIER STADIUMS
-        for(int i = 1; i <= 20; i ++){
-            //System.out.println(driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table[2]/tbody/tr["+i+"]/td[3]")).getText());
-            premierLeagueStadiums.add(driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table[2]/tbody/tr["+i+"]/td[3]")).getText());
+        for(int i = 2; i <= 83; i++){
+            rows = driver.findElements(By.xpath("//*[@id=\"mw-content-text\"]/div/table["+i+"]/tbody/tr"));
+            if(i == 54) i++;
+            for(int j = 2; j < rows.size(); j++){
+                if(i == 21 || i == 40 || i == 39 || i == 47 || i == 48 || i == 69 || i == 70){                                 //*[@id="mw-content-text"]/div/table[39]/tbody/tr[3]/td[2]
+                    stadiums.add(driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table["+i+"]/tbody/tr["+j+"]/td[2]")).getText());
+                }else {
+                    stadiums.add(driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table["+i+"]/tbody/tr["+j+"]/td[1]")).getText());
+                }
+            }
         }
 
-        //PRIMERA STADIUMS
-        for(int i = 1; i <= 20; i ++){
-            //System.out.println(driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table[2]/tbody/tr["+i+"]/td[3]")).getText());
-            primeraLeagueStadiums.add(driver.findElement(By.xpath("//*[@id=\"mw-content-text\"]/div/table[2]/tbody/tr["+i+"]/td[3]")).getText());
+        for(String a: stadiums){
+            System.out.println(a);
         }
 
-        System.out.println(premierLeagueStadiums);
-        System.out.println(primeraLeagueStadiums);
-
-        Thread.sleep(5000);
-        log.debug("Closing browser");
         driver.close();
         driver.quit();
+
+        System.out.println("UKUPNO STADIONA: "+stadiums.size());
+
+        try {
+
+            String myUrl = "jdbc:mysql://localhost:3306/footstats";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(myUrl, "root", "root");
+
+            String query = " Insert into stadium (name) values(?)";
+
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            conn.setAutoCommit(false);
+
+
+            for(String s: stadiums){
+                preparedStmt.setString(1, s);
+                preparedStmt.addBatch();
+            }
+            preparedStmt.executeBatch();
+            conn.commit();
+
+            conn.close();
+        } catch (Exception e) {
+            System.out.println("Got an exception!");
+            System.out.println(e.getMessage());
+        }
     }
 
-
-    /*@Test
-    public void getTwitterScreenshot() throws InterruptedException {
-     System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
-  WebDriver driver= new ChromeDriver();
-        driver.navigate().to("http://www.twitter.com/nike");
-        File scrFile = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
-        try {
-            FileUtils.copyFile(scrFile, new File("c:\\tmp\\screenshot.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        driver.close();
-        driver.quit();
-    }*/
 
 }
