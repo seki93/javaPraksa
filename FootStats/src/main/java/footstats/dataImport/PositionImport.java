b9695e5615d9f09503f5057639931db51ba2806f
ZekaJava <36188380+ZekaJava@users.noreply.github.com>
2018-04-13 12:04:40 +0200

    Zavrsen positionImport - iscitava listu pozicija i ubacuje u bazu

--- FootStats/src/main/java/footstats/dataImport/PositionImport.java
+++ FootStats/src/main/java/footstats/dataImport/PositionImport.java
@@ -3,15 +3,19 @@ package footstats.dataImport;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
 
 public class PositionImport {
 
     final static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(SekiSelenijum.class);
+
+    private ArrayList<String> positions = new ArrayList<>();
 
     @Test
     public void simpleChromeDrivertest() throws InterruptedException {
@@ -22,18 +26,46 @@ public class PositionImport {
         String url = "https://www.realbuzz.com/articles-interests/sports-activities/article/player-positions-in-soccer/";
         log.debug("navigating to the " + url);
         driver.get(url);
-        WebElement position = driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[1]"));
-
-        for(int i = 1;i <= 9; i++){
-
-            
-        }
 
         Actions actions = new Actions(driver);
-        actions.moveToElement(position);
-        actions.build().perform();
-        position.submit();
-        System.out.println(position.getText());
+
+        Thread.sleep(5000);
+
+        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[1]")).getText());
+        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[2]")).getText());
+        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[3]")).getText());
+        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[4]")).getText());
+        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[5]")).getText());
+        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[6]")).getText());
+        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[7]")).getText());
+        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[8]")).getText());
+
+        System.out.println(positions);
+
+        try {
+
+            String myUrl = "jdbc:mysql://localhost:3306/footstats";
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection conn = DriverManager.getConnection(myUrl, "root", "root");
+
+            String query = " Insert into position (name) values(?)";
+
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+
+            for(int i = 0;i < positions.size();i++) {
+
+                preparedStmt.setString(1, "" + positions.get(i));
+                preparedStmt.execute();
+
+            }
+
+            conn.close();
+
+        } catch (Exception e) {
+            System.out.println("Got an exception!");
+            System.out.println(e.getMessage());
+        }
+
         Thread.sleep(5000);
         log.debug("Closing browser");
         driver.close();
