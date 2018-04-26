b13fd5065e66664d382faa35015b4d1cbf29e89c
ZekaJava <36188380+ZekaJava@users.noreply.github.com>
2018-04-26 10:58:33 +0200

    Ispravka - PositionImport

--- FootStats/src/main/java/footstats/dataImport/PositionImport.java
+++ FootStats/src/main/java/footstats/dataImport/PositionImport.java
@@ -19,66 +19,12 @@ import java.util.ArrayList;
 public class PositionImport {
 
     final static org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(SekiSelenijum.class);
+
     private ArrayList<String> positions = new ArrayList<>();
 
     @Autowired
     PositionService positionService;
-
-    @Test
-    public void simpleChromeDrivertest() throws InterruptedException {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        log.debug("Opening browser");
-        driver.manage().window().maximize();
-        String url = "https://www.realbuzz.com/articles-interests/sports-activities/article/player-positions-in-soccer/";
-        log.debug("navigating to the " + url);
-        driver.get(url);
-
-        Actions actions = new Actions(driver);
-
-        Thread.sleep(5000);
-
-        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[1]")).getText());
-        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[2]")).getText());
-        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[3]")).getText());
-        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[4]")).getText());
-        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[5]")).getText());
-        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[6]")).getText());
-        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[7]")).getText());
-        positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3[8]")).getText());
-
-        System.out.println(positions);
-
-        try {
-
-            String myUrl = "jdbc:mysql://localhost:3306/footstats";
-            Class.forName("com.mysql.jdbc.Driver");
-            Connection conn = DriverManager.getConnection(myUrl, "root", "root");
-
-            String query = " Insert into position (name) values(?)";
-
-            PreparedStatement preparedStmt = conn.prepareStatement(query);
-
-            for(int i = 0;i < positions.size();i++) {
-
-                preparedStmt.setString(1, "" + positions.get(i));
-                preparedStmt.execute();
-
-            }
-
-            conn.close();
-
-        } catch (Exception e) {
-            System.out.println("Got an exception!");
-            System.out.println(e.getMessage());
-        }
-
-        Thread.sleep(5000);
-        log.debug("Closing browser");
-        driver.close();
-        driver.quit();
-    }
-
+    
     public void importPositions() throws  InterruptedException{
         System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -90,7 +36,7 @@ public class PositionImport {
 
         Actions actions = new Actions(driver);
 
-        for(int i = 1; i < 9; i++) {
+        for(int i = 1; i < 10; i++) {
             positions.add(driver.findElement(By.xpath("/html/body/div[4]/section/main/article/div[4]/div/div/h3["+i+"]")).getText());
         }
 
