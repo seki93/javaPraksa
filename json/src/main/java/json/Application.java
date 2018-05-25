fa70c7e494e002c698baa8bd733d1bce4499eac0
RajkoJegdic <rajkojegdic@yahoo.com>
2018-05-25 09:24:05 +0200

    Createt new thread for getting nonWorkingDays again part 2

--- json/src/main/java/json/Application.java
+++ json/src/main/java/json/Application.java
@@ -2,11 +2,19 @@ package json;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import json.Scheduler.CronTrigger;
+import org.quartz.SchedulerException;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+
+        try {
+            CronTrigger.createTrigger();
+        } catch (SchedulerException e) {
+            e.printStackTrace();
+        }
     }
 }
