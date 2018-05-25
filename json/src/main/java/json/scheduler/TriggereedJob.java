4d2c3cfad38b6655842c2261701d213e855d3928
RajkoJegdic <rajkojegdic@yahoo.com>
2018-05-25 09:18:15 +0200

    Createt new thread for getting nonWorkingDays again

---
+++ json/src/main/java/json/scheduler/TriggereedJob.java
@@ -0,0 +1,17 @@
+package json.Scheduler;
+
+import json.service.NonWorkingCalendarService;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class TriggereedJob implements Job {
+
+    @Autowired
+    NonWorkingCalendarService nonWorkingCalendarService;
+
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        nonWorkingCalendarService.createThread();
+    }
+}
